a9d08459b41a6522ad23df341a5402d3b576562f
yegabebar <m.barbegayet@gmail.com>
2021-03-17 19:19:53 +0100

    Stories 1 et 2 Ok

---
+++ logdump.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+logfile=$1
+destfolder=$2
+logname="Log-$(date '+%Y-%m-%d-%H-%M-%S').log"
+
+cp $logfile $destfolder/$logname && echo "" > $logfile && echo "$destfolder/$logname"
---
+++ logfilter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+logfile=$1
+#The first awk command allows us to keep only the lines matching "err" or "warn".
+#The second one is there to add a carriage return at the end of each line
+#The third awk removes the leading blankspaces added by the echo command.
+echo -e $(awk '/err|warn/' $logfile | awk '{print $0"\\n"}') | awk '{ sub(/^[ \t]+/, ""); print }'
