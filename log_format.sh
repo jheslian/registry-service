13c3f784d74f40309c6db27f00d7ae55f3ec6fe1
Jesusa SADEY <justinjhes@gmail.com>
2021-03-18 00:33:47 +0100

    recuperation message et un peu de commentaires

--- log_format.sh
+++ log_format.sh
@@ -5,17 +5,20 @@
 #fi
 cat $1 | while read line
 
-do
+# recover the date, type, message on each line
+do 
+    # filters the line that contains a date	
     if [[ $line == [??/??/????*  ]]; then
       	date=${line:1:19}
+        # recovers the type of a message
         if [[ $line == *warning* ]]; then
 	     type="warning"
 	elif [[ $line == *error* ]]; then
 	     type="error"			
-	fi      
-            # message=$line | cut -d':' -f4-  | awk '{ sub(/^[ \t]+/, ""); print }'
-	#	echo $($line | cut -d':' -f4-  | awk '{ sub(/^[ \t]+/, ""); print }')
-             echo "$date $type"
+	fi   
+             # acquire the message    
+             message=$(echo $line | cut -d':' -f4-  | awk '{ sub(/^[ \t]+/, ""); print }')
+             echo "$date $type $message"
     fi	
 
 done
