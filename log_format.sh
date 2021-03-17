781018b721253e6390b7850bb6c1943ba9abf5ee
Jesusa SADEY <justinjhes@gmail.com>
2021-03-18 00:09:57 +0100

    recuperation de date et type

--- log_format.sh
+++ log_format.sh
@@ -4,11 +4,20 @@
 #  echo "It's there!"
 #fi
 cat $1 | while read line
-do
-if [[ $line == [??/??/????*  ]]; then
-	echo $line
 
-fi	
+do
+    if [[ $line == [??/??/????*  ]]; then
+      	date=${line:1:19}
+        if [[ $line == *warning* ]]; then
+	     type="warning"
+	elif [[ $line == *error* ]]; then
+	     type="error"			
+	fi      
+            # message=$line | cut -d':' -f4-  | awk '{ sub(/^[ \t]+/, ""); print }'
+	#	echo $($line | cut -d':' -f4-  | awk '{ sub(/^[ \t]+/, ""); print }')
+             echo "$date $type"
+    fi	
 
 done
+
 
