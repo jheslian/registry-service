b58c33259c95bacf0ee7f1e18e86d0a434f0e0f9
yegabebar <m.barbegayet@gmail.com>
2021-03-18 15:36:50 +0100

    Script 1 modifié, modifs Dockerfile, reste à créer script 2 pour nohup: ajout modifs

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM registry.aptero.co/projet-linux
 COPY ./dockerscripts/ /dockerscripts/
+COPY ./html_root/ /var/www/html/
 CMD sh /dockerscripts/install_nginx_1.sh
+CMD sh /dockerscripts/install_nginx_2.sh
--- setupcontainer.sh
+++ setupcontainer.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-#docker pull registry.aptero.co/projet-linux:latest
+docker pull registry.aptero.co/projet-linux:latest
 sudo docker build -t registry.aptero.co/projet-linux .
-
-mapping=$1
-if [[ $mapping == ????:?? ]]; then
-	docker run -it --rm –p $2 --name projet-linux registry.aptero.co/projet-linux
-else
-	#Sera fonctionnel après création des deux scripts d'install nginx et inclusion dans dockerfile
-	#docker run -it --rm –p 8080:80 --name projet-linux registry.aptero.co/projet-linux
-	docker run -it --rm --name projet-linux registry.aptero.co/projet-linux
-fi
+sudo docker rm projet-linux 2> /dev/null
+docker run --name projet-linux registry.aptero.co/projet-linux
+#docker run -d –p 8080:80 projet-linux
