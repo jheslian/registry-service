e0dc3ac559b2d0775cbbad02e6b097cbfd5773a7
yegabebar <m.barbegayet@gmail.com>
2021-03-19 10:10:04 +0100

    Intégration move_log.sh dans container terminée, exec manuelle OK Story 5 validée

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM registry.aptero.co/projet-linux
 COPY ./dockerscripts/ /dockerscripts/
-RUN echo "echo OUFOYVDLDZAHDZAAZDOZAD" >> /workspace/log_server/startup.sh
 RUN sh /dockerscripts/install_nginx_2.sh
 RUN sh /dockerscripts/install_nginx_1.sh
 RUN sh /workspace/log_server/startup.sh
--- dockerscripts/log_move.sh
+++ dockerscripts/log_move.sh
@@ -6,17 +6,16 @@ mkdir -p $dailypath
 
 # Dump the log file to a daily folder and lists it in the dump_list file. 
 #Validates 1/, 2/, 3/
-bash ./log_dump.sh ./server.log $dailypath >> ./dump_list.txt
+bash ./log_dump.sh /workspace/log_server/server.log $dailypath >> ./dump_list.txt
 
 absolPathToFile=$(tail -n 1 ./dump_list.txt)
 logname=${absolPathToFile##*/}
 absolPathToJson="${absolPathToFile%.*}.json"
-archiveName="/var/www/html/${logname%.*}.tar.gz"
 #Validates 4/
-sudo bash ./log_filter.sh $absolPathToFile > ./tmp.log && sudo bash log_format.sh tmp.log >> $absolPathToJson
+bash ./log_filter.sh $absolPathToFile > ./tmp.log && bash ./log_format.sh ./tmp.log >> $absolPathToJson
 
 #Validates 5/
 echo $absolPathToJson >> ./json_list.txt
 
 #Validates 6/
-tar -cvzf $archiveName $dailypath
+tar -cvzf /var/www/html/log.tar.gz $dailypath
